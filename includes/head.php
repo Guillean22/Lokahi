4dbd53b1fe7ecf048fe4382c2fdad78194e565ff
Guillean22 <jean.rodrigue.stella@gmail.com>
2016-11-23 17:19:57 +0100

    day 3 final commit

--- includes/head.php
+++ includes/head.php
@@ -5,6 +5,6 @@
 		<link rel="stylesheet" href="/Projets/Lokahi/css/style.css">	<!-- création du lien vers la page css -->
 		<!-- <script type="text/javascript" src="/Projets/Lokahi/js/script.js"></script> --> <!-- création du lien vers la page javascript -->
 		<title>Lokahi</title>
-		<link rel="shortcut icon" href="/Projets/Lokahi/images/favicons/favicon.ico" type="image/x-icon">
-		<link rel="icon" href="/Projets/Lokahi/images/favicons/favicon.ico" type="image/x-icon">
+		<link rel="shortcut icon" href="/Projets/Lokahi/images/favicons/ico-range-lokahi-mini.png" type="image/x-icon">
+		<link rel="icon" href="/Projets/Lokahi/images/favicons/ico-range-lokahi-mini.png" type="image/x-icon">
 	</head>
